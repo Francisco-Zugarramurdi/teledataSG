7b4cbbda390273cab660e635bc29f701463cb84c
zzugarramurdi <franciscozugarramurdi@gmail.com>
2024-02-21 15:40:14 -0300

    response preliminar work

--- api.py
+++ api.py
@@ -9,6 +9,16 @@ import nest_asyncio
 
 app = Flask(__name__)
 
+def worked_ticket_response(ticket_id,priority,result):
+    url = "0.0.0.0:1111"
+    data = {
+        "ticket_id":ticket_id,
+        "priorty":priority,
+        "attention_log":result
+    }
+    requests.post(url,data)
+    
+
 @app.route("/set-ticket",methods=["POST","GET"])
 async def prueba():
     nest_asyncio.apply()
@@ -43,6 +53,7 @@ async def prueba():
     }
     result = await asyncio.gather(principal.manage_email(logs))
 
+    # worked_ticket_response(ticketId,priority,result)
     result = json.dumps(result)
     return result
 
